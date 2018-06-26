4174bc1868bc7d204082140840ab55b2a96591d1
guidmt <guim86@gmail.com>
2018-06-26 14:41:50 +0000

    Update run_freqmiss2.sh

--- run_freqmiss2.sh
+++ run_freqmiss2.sh
@@ -1,6 +1,6 @@
 export LC_ALL=C
 
-number_patients=$(pigz -dc /home/guidantonio/Scrivania/Exomes/ATVB_new/ATVB_WES.SNP.INDEL.pass.geno.chr7.vcf.bgz|sed -n '208p'|tr "\t" "\n"|tail -n+10|wc -l)
+number_patients=$(pigz -dc /dir/.vcf.gz|sed -n '208p'|tr "\t" "\n"|tail -n+10|wc -l)
 
 my_iteration=$(seq 10 $number_patients)
         
@@ -18,12 +18,12 @@ my_iteration=$(seq 10 $number_patients)
 
 		echo $i
 	#get the patient
-		current_patient=$(pigz -dc ../ATVB_new/ATVB_WES.SNP.INDEL.pass.geno.chr$i.vcf.gz|sed -n 208p |cut -f $pts)
+		current_patient=$(pigz -dc /dir/.vcf.gz|sed -n 208p |cut -f $pts)
 
 	#internal loop for the chromosomes
-	number_variant_for_sample=$(pigz -dc ../ATVB_new/ATVB_WES.SNP.INDEL.pass.geno.chr$i.vcf.gz|grep -v "#"|cut -f $pts|cut -d ":" -f 1|parallel --pipe --block 100M -L 50000 -P 6 grep -F -v "./."|wc -l) # -F fixed string -v invert selection
+	number_variant_for_sample=$(pigz -dc /dir/namevcf.chr$i.vcf.gz|grep -v "#"|cut -f $pts|cut -d ":" -f 1|parallel --pipe --block 100M -L 50000 -P 6 grep -F -v "./."|wc -l) # -F fixed string -v invert selection
 
-	number_miss_for_sample=$(pigz -dc ../ATVB_new/ATVB_WES.SNP.INDEL.pass.geno.chr$i.vcf.gz|cut -f $pts|cut -d ":" -f 1|parallel --pipe --block 100M -L 50000 -P 6 grep -F "./."|wc -l)
+	number_miss_for_sample=$(pigz -dc /dir/namevcf.chr$i.vcf.gz|cut -f $pts|cut -d ":" -f 1|parallel --pipe --block 100M -L 50000 -P 6 grep -F "./."|wc -l)
 
 	        echo	$current_patient	$i	$number_variant_for_sample	$number_miss_for_sample >> ../QC/freq_miss/$pts."freq_miss.txt"
 		
